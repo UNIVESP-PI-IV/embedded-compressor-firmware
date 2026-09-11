a5d81e477c71ed583db04ce9a2c48e74c875b21f
Ulisses <ualbardeiro@gmail.com>
2026-09-11 17:55:39 -0300

    feat(sensors): integra sensor de temperatura, checagem de conexao wifi e correcoes do cmake para esp-idf v6

--- main/sensors/temperature/temp_sensor.h
+++ main/sensors/temperature/temp_sensor.h
@@ -9,9 +9,7 @@ typedef struct {
 } temp_sensor_data_t;
 
 esp_err_t temp_sensor_init(void);
-
 esp_err_t temp_sensor_read(temp_sensor_data_t *data);
-
 char* temp_sensor_build_json(const temp_sensor_data_t *data);
 
 #endif 
